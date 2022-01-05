40f09857ba27a3dcaca66bc8a2431971493e5d76
Taz Nova <nova@Novas-Air.verizon.net>
2022-01-05 17:20:07 -0500

    updated

--- static/js/app.js
+++ static/js/app.js
@@ -21,3 +21,26 @@ data.forEach((dataRow) => {
         }
     );
 });
+
+// Function for button click
+function handleClick(){
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check if date was entered and filter the data using that date
+    if (date){
+        // Apply filter to table data to only keep rows where datetime matches filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild table using the filtered data
+    // Note: if no date was entered, then it'll just the be original tableData
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
